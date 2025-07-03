6c449d7218b5344ae33a6d81e045cea959d079fe
JoãoVitor <joaovitor01990@gmail.com>
2025-07-03 07:55:48 -0300

    Create questao10.cpp

---
+++ questao10.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+   void comparacao (float num1, float num2){
+       if (num1>num2){
+           cout<< num1 <<" e maior que "<< num2<<endl;
+       }else{
+           cout<< num2 <<" e maior que "<<num1<<endl;
+       }
+   }
